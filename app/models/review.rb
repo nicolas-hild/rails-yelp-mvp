651d16b3d74e42a2155bf58155621f2eb0d75f20
Nicolas Hild <hild_nicolas@yahoo.fr>
2019-11-13 14:51:11 +0100

    ok

---
+++ app/models/review.rb
@@ -0,0 +1,11 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating,
+            presence: true,
+            inclusion: {
+              in: (0..5).to_a
+            },
+            numericality: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'deleting restaurants...'
+Restaurant.destroy_all
+
+puts 'creating new restaurants...'
+
+restaurant = Restaurant.new(
+  name: 'Best restaurant ever',
+  address: 'Lille',
+  phone_number: '03 41 57 48 45',
+  category: 'japanese'
+)
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: 'patatas fritas',
+  address: 'Lille',
+  phone_number: '03 41 57 48 45',
+  category: 'belgian'
+)
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: 'chinese restaurant',
+  address: 'Lille',
+  phone_number: '03 41 57 48 45',
+  category: 'chinese'
+)
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: 'french restaurant',
+  address: 'Lille',
+  phone_number: '03 41 57 48 45',
+  category: 'french'
+)
+restaurant.save
+
+puts 'finished!'
